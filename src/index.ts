fd388b1871b61b0ca5e15820aaef871e26d96349
donghyun1225 <naokinaoki1225@gmail.com>
2023-03-01 00:16:59 +0900

    add aes crypto

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import CryptoJS from 'crypto-js';
+
 export const STORAGE = {
   LOCAL: 'LOCAL',
   SESSION: 'SESSION',
@@ -16,6 +18,47 @@ function SecureStorage(storage: string) {
     }
   };
 
+  const encrypt = (key: string, value: string, rounds?: number) => {
+    const salt = CryptoJS.lib.WordArray.random(128 / 8);
+
+    const hashedKey = CryptoJS.PBKDF2(key, salt, {
+      keySize: 512 / 32,
+      iterations: rounds ? rounds : 10000,
+    });
+
+    const iv = CryptoJS.lib.WordArray.random(128 / 8);
+
+    const encryptedValue = CryptoJS.AES.encrypt(value, hashedKey, {
+      iv: iv,
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC,
+    });
+
+    const encrypted = salt.toString() + iv.toString() + encryptedValue.toString();
+    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(encrypted));
+  };
+
+  const decrypt = (key: string, value: string, rounds?: number) => {
+    const encrypted = CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(value));
+
+    const salt = CryptoJS.enc.Hex.parse(encrypted.substring(0, 16 * 2));
+    const iv = CryptoJS.enc.Hex.parse(encrypted.substring(16 * 2, 16 * 2));
+    const encryptedValue = encrypted.substring(32 * 2);
+
+    const hashedKey = CryptoJS.PBKDF2(key, salt, {
+      keySize: 512 / 32,
+      iterations: rounds ? rounds : 10000,
+    });
+
+    const decrypted = CryptoJS.AES.decrypt(encryptedValue, hashedKey, {
+      iv: iv,
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC,
+    });
+
+    return decrypted;
+  };
+
   return {
     getItem: (key: string): string | null => {
       return getStorage(storage).getItem(key);
@@ -25,28 +68,3 @@ function SecureStorage(storage: string) {
     },
   };
 }
-
-// export const user = () => {
-//   const name = (): number => {
-//     // const value = 2;
-//     console.log('###########################################');
-//     return 3;
-//   };
-
-//   const email = (): number => {
-//     // const value = 2;
-//     console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-//     return 3;
-//   };
-
-//   // const test = (): number => {
-//   //   // const value = 2;
-//   //   console.log('$$$$$$$$$$$$$$$$$$$$$$$$');
-//   //   return 3;
-//   // };
-
-//   return { name: name, email: email };
-// };
-
-// const a = user();
-// a.name();
