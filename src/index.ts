65dfeb660087a4692c908237fa030d7c0f954c69
jihyunlab-dh <donghyun1114.kim@jihyunlab.com>
2024-05-23 22:34:09 +0900

    design interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export function main() {}
+export function WebSecureStorage(secret: string, iterations = 128) {
+  return {
+    local: {
+      clear: () => {
+        localStorage.clear();
+      },
+      getItem: (key: string) => {
+        localStorage.getItem(key);
+      },
+      setItem: (key: string, value: string) => {
+        localStorage.setItem(key, value);
+      },
+      removeItem: (key: string) => {
+        localStorage.removeItem(key);
+      },
+    },
+  };
+}
