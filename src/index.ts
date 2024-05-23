691dd768cd8c9938ccbfa9fdc93e925e3a783556
jihyunlab-dh <donghyun1114.kim@jihyunlab.com>
2024-05-23 23:29:48 +0900

    add error handling code

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,32 @@ export function WebSecureStorage(secret: string, iterations = 128) {
       clear: () => {
         localStorage.clear();
       },
+
       getItem: (key: string) => {
+        if (!key || key.length === 0) {
+          throw new Error('key does not exist.');
+        }
+
         localStorage.getItem(key);
       },
+
       setItem: (key: string, value: string) => {
+        if (!key || key.length === 0) {
+          throw new Error('key does not exist.');
+        }
+
+        if (!value || value.length === 0) {
+          throw new Error('value does not exist.');
+        }
+
         localStorage.setItem(key, value);
       },
+
       removeItem: (key: string) => {
+        if (!key || key.length === 0) {
+          throw new Error('key does not exist.');
+        }
+
         localStorage.removeItem(key);
       },
     },
