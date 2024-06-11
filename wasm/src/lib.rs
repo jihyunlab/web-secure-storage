3df141d01da22e89e4879f72a71f50f12174fd02
jihyunlab-dh <donghyun1114.kim@jihyunlab.com>
2024-06-11 23:48:19 +0900

    implement wasm

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,31 +3,93 @@ use aes_gcm::{
     Aes256Gcm, Key,
 };
 use pbkdf2::pbkdf2_hmac_array;
-use sha2::Sha256;
+use sha2::{digest::generic_array::GenericArray, Sha256};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn encrypt(text: &str, password: &str, salt: &str, iterations: u32) -> String {
+pub fn encrypt(text: &str, password: &str, salt: &str, iterations: u32) -> JsValue {
+    let len = text.len();
+
+    if len == 0 {
+        return JsValue::null();
+    }
+
+    let len = password.len();
+
+    if len == 0 {
+        return JsValue::null();
+    }
+
     let key = pbkdf2_hmac_array::<Sha256, 32>(password.as_bytes(), salt.as_bytes(), iterations);
     let key = Key::<Aes256Gcm>::from_slice(&key);
 
     let cipher = Aes256Gcm::new(&key);
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
-    let ciphertext = cipher.encrypt(&nonce, text.as_bytes()).unwrap();
 
-    let string = String::from_utf8(ciphertext).unwrap();
-    string
+    let ciphertext = match cipher.encrypt(&nonce, text.as_bytes()) {
+        Ok(encrypted) => encrypted,
+        _ => Vec::new(),
+    };
+
+    if ciphertext.len() == 0 {
+        return JsValue::null();
+    }
+
+    JsValue::from_str(&format!(
+        "{}{}",
+        hex::encode(nonce),
+        hex::encode(ciphertext)
+    ))
 }
 
 #[wasm_bindgen]
-pub fn decrypt(text: &str, password: &str, salt: &str, iterations: u32) -> String {
+pub fn decrypt(hex: &str, password: &str, salt: &str, iterations: u32) -> JsValue {
+    let len = hex.len();
+
+    if len <= 24 {
+        return JsValue::null();
+    }
+
+    let nonce_str = &hex[0..24];
+    let nonce_dec = match hex::decode(nonce_str) {
+        Ok(decoded) => decoded,
+        _ => Vec::new(),
+    };
+
+    if nonce_dec.len() == 0 {
+        return JsValue::null();
+    }
+
+    let nonce: &[u8] = &nonce_dec;
+
+    let text_str = &hex[24..len];
+    let text_dec = match hex::decode(text_str) {
+        Ok(decoded) => decoded,
+        _ => Vec::new(),
+    };
+
+    if text_dec.len() == 0 {
+        return JsValue::null();
+    }
+
+    let text: &[u8] = &text_dec;
+
     let key = pbkdf2_hmac_array::<Sha256, 32>(password.as_bytes(), salt.as_bytes(), iterations);
     let key = Key::<Aes256Gcm>::from_slice(&key);
 
     let cipher = Aes256Gcm::new(&key);
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
-    let plaintext = cipher.decrypt(&nonce, text.as_bytes()).unwrap();
 
-    let string = String::from_utf8(plaintext).unwrap();
-    string
+    let plaintext = match cipher.decrypt(&GenericArray::from_slice(nonce), text) {
+        Ok(decrypted) => decrypted,
+        _ => Vec::new(),
+    };
+
+    if plaintext.len() == 0 {
+        return JsValue::null();
+    }
+
+    JsValue::from_str(&format!(
+        "{}",
+        String::from_utf8(plaintext).unwrap().to_string()
+    ))
 }
